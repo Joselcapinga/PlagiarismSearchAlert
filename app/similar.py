1c97ba67630a668baf08fdff25f4dd30e02bc3ee
josecapinga <josecapinga@alunos.utfpr.edu.br>
2023-10-02 21:37:06 -0300

    tratamento de exceção para google search

--- app/similar.py
+++ app/similar.py
@@ -4,7 +4,12 @@ class Similar():
 
     def LinksSimilar(self, query, numLinks = 5):
         
-        search_results = []
-        for i in search(query, num= 10, stop= numLinks, pause= 2):
-            search_results.append(i)
-        return search_results
+       search_results = []
+        
+       try:
+            for i in search(query, num= 10, stop= numLinks, pause= 2):
+                search_results.append(i)
+            return search_results
+       except:
+           
+           return search_results
